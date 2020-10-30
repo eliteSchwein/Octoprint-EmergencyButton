57a67bc6d6d3917c68f1e1f06f5595a058dce8ac
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2020-10-30 21:59:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "filamentreload"
+plugin_identifier = "emergencybutton"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_filamentreload"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Octoprint-filamentreload"
+plugin_name = "Octoprint-emergencybutton"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.3.3"
+plugin_version = "1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """A better version of the filament monitor that actually pauses the print when your filament runs out"""
+plugin_description = """A Configurable Emergency Button"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Adrià Cereto i Massagué"
+plugin_author = "eliteSCHW31N"
 
 # The plugin's author's mail address.
-plugin_author_email = "ssorgatem@gmail.com"
+plugin_author_email = "business@eliteschw31n.de"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/ssorgatem/Octoprint-Filament-Reloaded"
+plugin_url = "https://github.com/eliteSchwein/Octoprint-EmergencyButton"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
