c508b2f0a6a719b80aa6c72a76bb9ee356a154fe
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2020-10-30 22:40:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_emergencybutton"
 plugin_name = "Octoprint-EmergencyButton"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.1"
+plugin_version = "R1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
